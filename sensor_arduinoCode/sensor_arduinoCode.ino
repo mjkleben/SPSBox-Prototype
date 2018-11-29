ea8a19d5c2a900607f88b2529d79ddfa41277b5f
min <mjkleben@gmail.com>
2018-11-28 19:40:04 -0500

    serial number confirmation and email sending works now

---
+++ sensor_arduinoCode/sensor_arduinoCode.ino
@@ -0,0 +1,26 @@
+
+int trigPin = 9;
+int echoPin = 10;
+
+
+void setup() {
+  Serial.begin(9600); 
+   pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  delay(100);
+
+ 
+  Serial.print(distance);
+  Serial.println("CM");
+ }
